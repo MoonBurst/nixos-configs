aa12fe453a9aaa72a1c44b751f57ceb234a6d3bb
Moon Burst <MoonBurstPlays@gmail.com>
2025-10-27 01:56:23 -0500

    Added common module to Git tracking.

---
+++ modules/common/default.nix
@@ -0,0 +1,232 @@
+# ~/nixos-config/modules/common/default.nix
+{ config, pkgs, lib, ... }:
+
+{
+  # Set your time zone.
+  time.timeZone = "America/Matamoros";
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  # This value determines the NixOS release.
+  system.stateVersion = "25.05"; 
+  
+  # ====================================================================
+  # NETWORKING
+  # ====================================================================
+  networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
+  
+  # ====================================================================
+  # SERVICES AND HARDWARE
+  # ====================================================================
+
+  #---Graphics/Display ---
+  services.xserver.enable = true; 
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
+  programs.corectrl.enable = true;
+  security.polkit.enable = true;
+  services.dbus.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.sway.enableGnomeKeyring = true;
+ 
+ 
+  #--- Display Manager
+  services.displayManager.ly.enable = true;  
+ 
+  # --- Audio: PipeWire (Full Setup) ---
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # --- Wayland/App Compatibility ---
+
+ 
+  # --- XDG Portal Configuration ---
+  xdg.portal = {
+    enable = true;
+    configPackages = [ pkgs.xdg-desktop-portal-wlr ]; 
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ]; 
+  };
+  
+
+  # ====================================================================
+  # PROGRAMS, SHELLS, and THEME FIXES
+  # ====================================================================
+  
+  # Sway
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true; 
+  };
+
+  # Zsh configuration
+  programs.zsh.enable = true;
+
+  # GnuPG / SSH Agent
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # Qt/GTK Theming Fix
+  environment.sessionVariables = {
+    QT_QPA_PLATFORMTHEME = "qt5ct"; 
+    GTK_THEME="Moon-Burst-Theme";
+    GDK_BACKEND = "wayland,x11"; 
+  };
+  # 1. Shared System-Wide Settings
+  environment.systemPackages = with pkgs; [
+    git # A tool everyone needs
+    neofetch
+  ];
+  
+  # ====================================================================
+  # USER CONFIGURATION
+  # ====================================================================
+  users.users.moonburst = {
+    isNormalUser = true;
+    description = "MoonBurst";
+    extraGroups = [ 
+      "networkmanager" 
+      "wheel" 
+      "audio" 
+      "video" 
+      "input"
+      "render"
+      "corectrl"
+      "i2c"
+    ];
+    
+    shell = pkgs.zsh;
+  };
+  # ====================================================================
+  # FONTS
+  # ====================================================================
+   fonts = {
+    packages = with pkgs; [
+      fira-sans
+      font-awesome
+      roboto
+      nerd-fonts._0xproto
+      nerd-fonts.droid-sans-mono
+      nerd-fonts.jetbrains-mono
+      jetbrains-mono
+      noto-fonts
+      noto-fonts-emoji
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      material-symbols
+      material-icons
+	  ];
+    fontconfig.enable = true;
+    };
+  # ====================================================================
+  # ENVIRONMENT AND PACKAGES
+  # ====================================================================
+  nixpkgs.config.allowUnfree = true;
+	nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+  
+  # ====================================================================
+  # APPIMAGE STUFF
+  # ====================================================================
+  programs.appimage = {
+    enable = true;
+    binfmt = true; 
+    package = pkgs.appimage-run.override {
+      extraPkgs = p: [
+        p.xorg.libxshmfence
+   
+        # p.libxshmfence32
+        
+      ];
+    };
+  };
+  
+  # ====================================================================
+  # NIX-LD
+  # ====================================================================
+    programs.nix-ld.enable = true;
+    programs.nix-ld.libraries = with pkgs; [
+    #put programs here
+    ];
+  
+	environment.systemPackages = with pkgs; [
+    # --- System Utilities/Shell
+    kitty
+    fastfetch
+    gnome-system-monitor
+    s-tui
+    nano
+    git
+    github-cli
+    gnupg
+    jq
+    bc
+    rsync
+    rclone
+    procps
+    psmisc
+    gawk
+    ripgrep
+    dict
+    libsecret
+    kdePackages.polkit-kde-agent-1
+    linux-firmware
+    nix-prefetch-github
+
+    # --- Btrfs Tools 
+    btrfs-progs
+
+    # --- Wayland Utilities 
+    waybar
+    grim
+    slurp
+    wl-clipboard
+    satty
+    wtype
+    playerctl
+    dunst
+    swaylock
+    swayidle
+
+    # --- Desktop/Theming
+    nemo
+    lxqt.pavucontrol-qt
+    bluez-tools
+    qt5.qtwayland
+    qt6ct
+
+    # --- Applications/Communication
+    audacious
+    vivaldi
+    vesktop
+    evolution
+
+    # --- Other Tools
+    pass
+    unar
+    zip
+    unzip
+    sox
+    geany
+    sherlock-launcher
+    cliphist
+    fchat-horizon
+    moon-burst-theme
+  
+  
+]
+
+
+  # ... other shared system settings
+}
