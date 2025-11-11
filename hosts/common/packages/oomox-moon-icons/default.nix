1acc8ea2de4fde1471c75f0e16623d7967c9904d
Moon Burst <MoonBurstPlays@gmail.com>
2025-11-11 11:28:34 -0600

    fix: Define stdenv argument in icon package derivation.

--- hosts/common/packages/oomox-moon-icons/default.nix
+++ hosts/common/packages/oomox-moon-icons/default.nix
@@ -1,8 +1,10 @@
+{ stdenv, pkgs }:
+
 stdenv.mkDerivation {
   pname = "oomox-moon-icon-theme";
   version = "1.0";
 
-  src = ./.; # Source is the current directory
+  src = ./.; 
   themeDirName = "oomox-Moon Theme"; 
 
   installPhase = ''
