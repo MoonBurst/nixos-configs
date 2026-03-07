20caffedaa146ab9e4a4b0d23994bc03570c2afc
Moon Burst <MoonBurstPlays@gmail.com>
2026-03-07 02:37:33 -0600

    Fix syntax error in users.nix

--- hosts/common/users.nix
+++ hosts/common/users.nix
@@ -3,28 +3,6 @@
 {
   # --- Main User Account ---
   users.users.moonburst = {
-    # Password managed via SOPS in security.nix
-    hashedPasswordFile = "/run/secrets/sops_key";
-    isNormalUser = true;
-    description = "MoonBurst";
-    home = "/home/moonburst";
-
-    # Permissions and hardware access
-    extraGroups = [
-      "networkmanager" "wheel" "audio" "video" "input"
-      "render" "corectrl" "i2c" "libvirtd"
-    ];
-
-    # Default Shell
-    shell = pkgs.zsh;
-
-    # Authorized keys are managed via sops-nix in security.nix
-  };
-}{ config, pkgs, ... }:
-
-{
-  # --- Main User Account ---
-  users.users.moonburst = {
     # FIXED: Pointed to the new master_password path from SOPS
     hashedPasswordFile = config.sops.secrets.master_password.path;
 
@@ -44,4 +22,3 @@
     # Authorized keys are managed via sops-nix in security.nix
   };
 }
-
