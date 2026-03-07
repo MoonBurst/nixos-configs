975d8a17c1f03acbcf399ce5d4c1c1a1d926a35e
Moon Burst <MoonBurstPlays@gmail.com>
2026-03-07 02:36:46 -0600

    Final fix: Update user password reference to master_password

--- hosts/common/users.nix
+++ hosts/common/users.nix
@@ -20,4 +20,28 @@
 
     # Authorized keys are managed via sops-nix in security.nix
   };
+}{ config, pkgs, ... }:
+
+{
+  # --- Main User Account ---
+  users.users.moonburst = {
+    # FIXED: Pointed to the new master_password path from SOPS
+    hashedPasswordFile = config.sops.secrets.master_password.path;
+
+    isNormalUser = true;
+    description = "MoonBurst";
+    home = "/home/moonburst";
+
+    # Permissions and hardware access
+    extraGroups = [
+      "networkmanager" "wheel" "audio" "video" "input"
+      "render" "corectrl" "i2c" "libvirtd"
+    ];
+
+    # Default Shell
+    shell = pkgs.zsh;
+
+    # Authorized keys are managed via sops-nix in security.nix
+  };
 }
+
