96874528834dbafa0b5c62e85cf969df651bc38c
Moon Burst <MoonBurstPlays@gmail.com>
2026-03-16 03:18:44 -0500

    moving borg cache

--- hosts/moonbeauty/services.nix
+++ hosts/moonbeauty/services.nix
@@ -5,6 +5,9 @@ let
     ${pkgs.coreutils}/bin/cat ${config.sops.secrets.borg_passphrase.path}
   '';
   rcloneConfigPath = "/run/rclone-mount/nextcloud.conf";
+  # Centralized cache location on your 3TB HDD
+  rcloneCacheDir = "/mnt/3TBHDD/rclone-cache";
+
   baseExcludes = [
     "*/.config/BraveSoftware"
     "*/.cache/BraveSoftware"
@@ -62,6 +65,8 @@ in
       ExecStartPre = lib.mkForce (pkgs.writeShellScript "prep-nextcloud-mount" ''
         export PATH=$PATH:${pkgs.glibc.bin}/bin
         ${pkgs.coreutils}/bin/mkdir -p /mnt/nextcloud
+        ${pkgs.coreutils}/bin/mkdir -p ${rcloneCacheDir}
+
         RAW_URL=$(${pkgs.coreutils}/bin/cat ${config.sops.secrets.nextcloud_url.path} | ${pkgs.coreutils}/bin/tr -d '[:space:]')
         USER=$(${pkgs.coreutils}/bin/cat ${config.sops.secrets.nextcloud_user.path} | ${pkgs.coreutils}/bin/tr -d '[:space:]')
         PASS=$(${pkgs.coreutils}/bin/cat ${config.sops.secrets.nextcloud_pass.path} | ${pkgs.coreutils}/bin/tr -d '[:space:]')
@@ -70,6 +75,7 @@ in
 
         BASE_URL=$(echo "$RAW_URL" | ${pkgs.gnused}/bin/sed 's|/*$||')
         FINAL_URL="$BASE_URL/remote.php/dav/files/$USER/"
+
         echo "[NextCloud]
         type = webdav
         vendor = nextcloud
@@ -78,7 +84,30 @@ in
         pass = $OBSCURED_PASS" > ${rcloneConfigPath}
         ${pkgs.coreutils}/bin/chmod 600 ${rcloneConfigPath}
       '');
-      ExecStart = lib.mkForce "${pkgs.rclone}/bin/rclone mount NextCloud: /mnt/nextcloud --config ${rcloneConfigPath} --vfs-cache-mode writes --vfs-cache-max-age 1s --vfs-write-back 0s --dir-cache-time 1s --attr-timeout 1s --webdav-nextcloud-chunk-size 4M --allow-non-empty --allow-other --rc --stats 5s";
+
+      # FIXES APPLIED BELOW:
+      # 1. Added --cache-dir pointing to your 3TB HDD
+      # 2. Changed mode to 'full' for better Borg compatibility
+      # 3. Increased write-back delay to 5s to prevent "file not found" errors
+      # 4. Increased cache-max-age and dir-cache-time to stop rclone from expiring files too fast
+      # 5. Set chunk size to 10M to balance speed and avoid Cloudflare 524 timeouts
+      ExecStart = lib.mkForce ''
+        ${pkgs.rclone}/bin/rclone mount NextCloud: /mnt/nextcloud \
+          --config ${rcloneConfigPath} \
+          --cache-dir ${rcloneCacheDir} \
+          --vfs-cache-mode full \
+          --vfs-cache-max-size 100G \
+          --vfs-cache-max-age 24h \
+          --vfs-write-back 5s \
+          --dir-cache-time 5m \
+          --attr-timeout 5m \
+          --webdav-nextcloud-chunk-size 10M \
+          --allow-non-empty \
+          --allow-other \
+          --rc \
+          --stats 5s
+      '';
+
       ExecStop = lib.mkForce "${pkgs.fuse}/bin/fusermount -uz /mnt/nextcloud";
       Restart = "on-failure";
     };
