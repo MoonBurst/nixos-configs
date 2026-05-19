4a6c5cc8d8ca563af9b78a9bc6c8b8af6fa1e3e2
Moon Burst <MoonBurstPlays@gmail.com>
2026-05-19 18:54:53 -0500

    updating quickshell. notifications are still broken on action/dismiss

--- hosts/common/programs/hyprland/autostart.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }: ''
-  exec-once = ${pkgs.brave}/bin/brave
-  exec-once = ${pkgs.vesktop}/bin/vesktop
-  exec-once = ${pkgs.corectrl}/bin/corectrl
-  exec-once = ${pkgs.quickshell}/bin/quickshell --path /home/moonburst/nix/hosts/common/programs/quickshell/shell.qml
-  exec-once = ${pkgs.wl-clipboard}/bin/wl-paste --type text --watch ${pkgs.cliphist}/bin/cliphist store -max-items 50
-  exec-once = ${pkgs.wl-clipboard}/bin/wl-paste --type image --watch ${pkgs.cliphist}/bin/cliphist store -max-items 10
-''
