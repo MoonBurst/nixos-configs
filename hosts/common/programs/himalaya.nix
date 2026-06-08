f55e087dc68d1ea2eaee4e5634c4f78ef09982b4
Moon Burst <MoonBurstPlays@gmail.com>
2026-06-08 14:39:55 -0500

    fix: permanent correction of server hostname strings

--- hosts/common/programs/himalaya.nix
+++ hosts/common/programs/himalaya.nix
@@ -127,7 +127,7 @@ in {
 
       [accounts.gmail.message.send.backend]
       type = "smtp"
-      host = "://gmail.com"
+      host = "smtp.gmail.com"
       port = 465
       auth.type = "password"
       login = "$HIMALAYA_GMAIL_ADDRESS"
@@ -150,7 +150,7 @@ in {
       SyncState *
 
       IMAPAccount gmail
-      Host ://gmail.com
+      Host imap.gmail.com
       Port 993
       UserCmd "echo $HIMALAYA_GMAIL_ADDRESS"
       PassCmd "echo $HIMALAYA_GMAIL_PASSWORD"
